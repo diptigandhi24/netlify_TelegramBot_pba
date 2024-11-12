86f4ebfe627f90e77ad424e942e77fa322ae2044
diptigandhi24 <gandhidipti.s@gmail.com>
2024-11-12 16:45:19 +0530

    logging the request recieved by netlify function

--- @netlify/functions/hello-world/hello-world.ts
+++ @netlify/functions/hello-world/hello-world.ts
@@ -1,8 +1,11 @@
 import { Handler } from "@netlify/functions";
 
-export const handler: Handler = async (request, context) => {
-  console.log("REceived a call with: ", request.body);
+export const handler: Handler = async (request) => {
+  console.log("Recieved request", request);
   return {
     statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, from Dipti!`,
+    }),
   };
 };
