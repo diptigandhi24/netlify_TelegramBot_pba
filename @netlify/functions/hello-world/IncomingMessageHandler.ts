dd5ffac6398db44f87dfb28c4cab6a6c8ea24f05
diptigandhi24 <gandhidipti.s@gmail.com>
2024-11-13 10:18:48 +0530

    rename the file and function

--- @netlify/functions/hello-world/IncomingMessageHandler.ts
+++ @netlify/functions/hello-world/IncomingMessageHandler.ts
@@ -4,8 +4,5 @@ export const handler: Handler = async (request) => {
   console.log("Recieved request", request);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, from Dipti!`,
-    }),
   };
 };
