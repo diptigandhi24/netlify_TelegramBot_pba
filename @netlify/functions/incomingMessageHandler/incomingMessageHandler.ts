45a5fdff1cca46ca17fd79970a097c59a77bcff4
diptigandhi24 <gandhidipti.s@gmail.com>
2024-11-20 15:51:23 +0530

    printing the json received from the server

--- @netlify/functions/incomingMessageHandler/incomingMessageHandler.ts
+++ @netlify/functions/incomingMessageHandler/incomingMessageHandler.ts
@@ -1,6 +1,16 @@
 import { Handler } from "@netlify/functions";
+const { MongoClient } = require("mongodb");
 
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const myDB = mongoClient.db("pba");
+const myColl = myDB.collection("parents_questions");
+
+// make a mondo db call
+async function postToDB(data) {
+  console.log("message we receive from instagram", data.body.message.text);
+}
 export const handler: Handler = async (request) => {
+  postToDB(request);
   return {
     statusCode: 200,
   };
