9b68b5faf57d0bd4ccd6019f93ffa0b39bd33c83
diptigandhi24 <gandhidipti.s@gmail.com>
2024-12-03 17:04:19 +0530

    removing mongodb call from FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { getCollection } from "../collectionInstance";
-import { DBResponse } from "../messageTypes";
-import * as mongoDB from "mongodb";
 
 function App() {
-  const [documentList, UpdateDocumentList] = useState("Loading");
-
-  useEffect(async () => {
-    let collection: DBResponse = await getCollection();
-    if (collection.statusCode === 200) {
-      const myColl = collection.response;
-      console.log("response received from collection", myColl);
-    }
-  }, []);
-
   return (
     <>
       <div>
