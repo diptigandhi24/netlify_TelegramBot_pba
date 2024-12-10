a0bda563e5f58030871f9b80eb3d1b55217d642e
diptigandhi24 <gandhidipti.s@gmail.com>
2024-12-10 12:42:03 +0530

    added console log

--- @netlify/functions/incomingMessageHandler/updateData.ts
+++ @netlify/functions/incomingMessageHandler/updateData.ts
@@ -1,6 +1,7 @@
 import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (request: object) => {
+  console.log("Update function is active");
   return {
     statusCode: 200,
   };
