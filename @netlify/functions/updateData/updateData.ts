923436999d12ccbed32cd503eb3986b1d5939055
diptigandhi24 <gandhidipti.s@gmail.com>
2024-12-10 17:33:19 +0530

    resolving CORS issue

--- @netlify/functions/updateData/updateData.ts
+++ @netlify/functions/updateData/updateData.ts
@@ -1,11 +1,14 @@
 import { Handler } from "@netlify/functions";
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+};
 export const handler: Handler = async (request: object) => {
   console.log("Update function is active", request);
   return {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Allow from anywhere
-    },
+    headers,
   };
 };
