d0a0935763ef9edbf726fd10edcf77f6396ff2a1
diptigandhi24 <gandhidipti.s@gmail.com>
2024-12-10 17:11:28 +0530

    adding cors support

--- @netlify/functions/updateData/updateData.ts
+++ @netlify/functions/updateData/updateData.ts
@@ -4,5 +4,8 @@ export const handler: Handler = async (request: object) => {
   console.log("Update function is active", request);
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Allow from anywhere
+    },
   };
 };
