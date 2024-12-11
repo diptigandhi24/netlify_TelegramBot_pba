26f8b29bcce43e98669c2a39df0a1721d5b134fc
diptigandhi24 <gandhidipti.s@gmail.com>
2024-12-11 10:26:57 +0530

    removing unused import

--- mongodb/collectionInstance.ts
+++ mongodb/collectionInstance.ts
@@ -1,6 +1,5 @@
 const { MongoClient } = require("mongodb");
 import * as mongoDB from "mongodb";
-import { QuestionAnswer } from "../netlifyUtility/messageTypes";
 const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
 export async function getCollection(): Promise<mongoDB.Collection> {
